9e0e9edec2fdbd1b56b50041cdeec26a5d607bf1
Adoria298 <bananaorange29@outlook.com>
2020-04-19 19:10:40 +0000

    Moved playsound out of the function.

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from playsound import playsound
 from gpiozero import Button
 from signal import pause
 
+playsound("./WhoDisturbsMySlumber.wav")
+
 button = Button(2)
 
-def play_line():
+def shutdown():
     """
-    Plays './WhoDisturbsMySlumber.wav'.
+    Pretends to shut down.
     """
     print("Button pressed.")
-    playsound("./WhoDisturbsMySlumber.wav")
-    print("Played.")
 
-button.when_pressed = play_line
+button.when_pressed = shutdown
 
 pause()
