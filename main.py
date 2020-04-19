7e32e030a2170807e43a411e42090cffb2cc30bb
Adoria298 <bananaorange29@outlook.com>
2020-04-19 19:29:06 +0000

    Made the button make the Pi reboot.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from playsound import playsound
 from gpiozero import Button
 from signal import pause
+import os
 
 playsound("./WhoDisturbsMySlumber.wav")
+print("Played sound.")
 
 button = Button(2)
 
 def shutdown():
     """
-    Pretends to shut down.
+    Reboots on button press.
     """
     print("Button pressed.")
+    os.system("sudo shutdown -r now")
 
 button.when_pressed = shutdown
 
